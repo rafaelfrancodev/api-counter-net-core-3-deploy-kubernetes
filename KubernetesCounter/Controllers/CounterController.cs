02b172c7fb0ce0296a66d561e24108e7728c3100
Rafael Franco <rafael.apfsantos@outlook.com>
2020-06-10 09:56:11 -0300

    Add project files.

---
+++ KubernetesCounter/Controllers/CounterController.cs
@@ -0,0 +1,29 @@
+﻿using KubernetesCounter.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace KubernetesCounter.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CounterController : Controller
+    {
+        private static Counter _COUNTER = new Counter();
+
+        [HttpGet]
+        public object Get()
+        {
+            lock(_COUNTER)
+            {
+                _COUNTER.Increment();
+
+                return new
+                {
+                    _COUNTER.CurrentValue,
+                    Environment.MachineName,
+                    OSVersion = Environment.OSVersion.VersionString
+                };
+            }
+        }
+    }
+}
---
+++ KubernetesCounter/Models/Counter.cs
@@ -0,0 +1,13 @@
+﻿namespace KubernetesCounter.Models
+{
+    public class Counter
+    {
+        private int _currentValue = 0;
+        public int CurrentValue { get => _currentValue;  }
+
+        public void Increment()
+        {
+            _currentValue++;
+        }
+    }
+}
